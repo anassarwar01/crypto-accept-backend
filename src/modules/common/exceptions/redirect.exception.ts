903e6e01574ee03842f012643dc8780c5a822fbb
Anas Sarwar <anas.sarwar@developers.studio>
2026-03-05 16:01:50 +0500

    Update http status

--- src/modules/common/exceptions/redirect.exception.ts
+++ src/modules/common/exceptions/redirect.exception.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class RedirectException extends HttpException {
-    constructor(public readonly url: string, status: HttpStatus = HttpStatus.CONFLICT) {
+    constructor(public readonly url: string, status: HttpStatus = HttpStatus.PERMANENT_REDIRECT) {
         super('Redirecting...', status);
     }
 }
